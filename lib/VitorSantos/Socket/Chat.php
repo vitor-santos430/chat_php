0a9210404841d8e7b2e04b28190baa043e06f395
Vitor Santos <vitor.santos430@etec.sp.gov.br>
2020-08-12 20:08:10 -0300

    Classe principal do chat e Alias Vi no compos.json

---
+++ lib/VitorSantos/Socket/Chat.php
@@ -0,0 +1,20 @@
+<?php
+    //Vi é o alias
+    namespace Vi\Socket;
+
+    use Ratchet\MessageComponentInterface;
+    use Ratchet\ConnectionInterface;
+
+    class Chat implements MessageComponentInterface {
+        public function onOpen(ConnectionInterface $conn) {
+        }
+
+        public function onMessage(ConnectionInterface $from, $msg) {
+        }
+
+        public function onClose(ConnectionInterface $conn) {
+        }
+
+        public function onError(ConnectionInterface $conn, \Exception $e) {
+        }
+    }
